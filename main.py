9c4302845fa35bd4cdb2673f58246386606070a5
Holden Galusha <holden.galusha@gmail.com>
2018-01-04 20:06:05 -0500

    adds main.py

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
 
 import room_classes as rc
+import reservation_class as reservation
 import pickle
+import random
+import available_rooms as ar
 
+#This is the list of current unavailable rooms
+occupied_budget_rooms = []
+occupied_single_rooms = []
+occupied_family_rooms = []
+occupied_penthouses = []
+
+'''
 def recommendRoom(x):
 	if x <= 2:
 		print("We recommend the Single Bed Suite.")
@@ -14,9 +24,24 @@ def recommendRoom(x):
 	elif x >= 8:
 		print("We recommend the Penthouse Suite.")
 		rc.Room.Penthouse.display_info()
-
+'''
 print("Welcome!")
-occupants_quantity = input("Please specify the number of occupants you would like to reserve rooms for: ")
-recommendRoom(occupants_quantity)
 
+#This function performs room reservation
+def reserve_room():
 
+	def determine_room_number(x):
+		if x == 'budget':
+			occupied_budget_rooms.append(reservation_room_number)
+
+		elif x == 'single':
+			occupied_single_rooms.append(reservation_room_number)
+
+		elif x == 'family':
+			occupied_family_rooms.append(reservation_room_number)
+
+	reservation_surname = input("What is your name?\n>>")
+	reservation_room_type = input("What is your preferred room type?\n>>")
+	determine_room_number(reservation_room_type)
+	reservation_occupants = input("How many occupants will be in the room?\n>>")
+	reservation_date = input("What is your reservation date? (MM/DD/YY-MM/DD/YY)\n>>")
