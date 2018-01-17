140bc6453ed2fc17cf5d7a95eb972ac7b2bb8dff
Holden Galusha <holden.galusha@gmail.com>
2018-01-17 00:18:30 -0500

    fixed bug, 'improved' functionality

--- main.py
+++ main.py
@@ -2,32 +2,67 @@
 
 import random
 import sqlite3
+import pickle
 
 database_connection = sqlite3.connect('reservations.db')
 cursor = database_connection.cursor()
 cursor.execute('CREATE TABLE IF NOT EXISTS reservations (surname, room_type, room_number, occupants, date)')
 
-budget = [11,22,33,44,55,66,77,88,99]
-single = [100,102,104,106,108,110,112,114,116,118,120,124,128,130,132]
-family = [200,202,204,206,208,210,212,214,216]
-penthouse = [900,902,904,906,908,910]
-
-print("Welcome!\n\n")
+print("\nWelcome!\n\n")
 
 occupied_rooms = []
 
+#This should only be run once--put in separate setup file
+def initial_pickling():
+	budget = [11,22,33,44,55,66,77,88,99]
+	with open('budget.pickle','wb') as pickled_budget:
+		pickle.dump(budget,pickled_budget)
+
+	single = [100,102,104,106,108,110,112,114,116,118,120,124,128,130,132]
+	with open('single.pickle','wb') as pickled_single:
+		pickle.dump(single,pickled_single)
+
+	family = [200,202,204,206,208,210,212,214,216]
+	with open('family.pickle','wb') as pickled_family:
+		pickle.dump(family,pickled_family)
+
+	penthouse = [900,902,904,906,908,910]
+	with open('penthouse.pickle','wb') as pickled_penthouse:
+		pickle.dump(penthouse,pickled_penthouse)
+
+	print("Done pickling!")
+#initial_pickling()
+
 #This function determines room number
 def determine_room_number(x):
-	room_number = random.choice(eval(x))
-	occupied_rooms.append(room_number)
-	eval(x).remove(room_number)
+	with open(x+'.pickle','rb') as available_rooms_in:
+		available_rooms = pickle.load(available_rooms_in)
+
+	room_number = random.choice(available_rooms)
+	available_rooms.remove(room_number)
+
+	with open('occupied_rooms.pickle','wb') as occupied_rooms:
+		pickle.dump(room_number,occupied_rooms)
+
+	print(available_rooms)
+	print(room_number)
 
 #This function performs room reservation
 def reserve_room():
 	room_type = input("What is your preferred room type? (Budget, Single, Family, Penthouse)\n>>")
 	room_type = room_type.lower()
-	determine_room_number(room_type)
-	room_number = occupied_rooms[-1]
+
+	with open(room_type+'.pickle','rb') as available_rooms_in:
+		available_rooms = pickle.load(available_rooms_in)
+
+	room_number = random.choice(available_rooms)
+	available_rooms.remove(room_number)
+
+	with open('occupied_rooms.pickle','wb') as occupied_rooms:
+		pickle.dump(room_number,occupied_rooms)
+
+	print(available_rooms)
+	print(room_number)
 	occupants = input("How many occupants will be in the room?\n>>")
 	surname = input("What is your name?\n>>")
 	date = input("What is your reservation date? (MM/DD/YY-MM/DD/YY)\n>>")
@@ -44,7 +79,17 @@ def display_sql_res_info():
 			print(row)
 	else:
 		print("No such record found.")
+
+def reopen_room(y,z):
+	with open(y+'.pickle','wb') as open_rooms:
+		pickle.dump(z, open_rooms)
+
 def delete_reservation(x):
+	cursor.execute("SELECT room_number, room_type FROM reservations WHERE surname=?", (x,))
+	data = cursor.fetchone()
+	room_number_to_reopen = str(data[0])
+	room_type_to_reopen = data[1]
+	reopen_room(room_type_to_reopen, room_number_to_reopen)
 	cursor.execute("DELETE FROM reservations WHERE surname=?", (x,))
 	database_connection.commit()
 	print("Successful deletion.")
@@ -91,7 +136,7 @@ def decide_action():
 	elif int(potential_action) == 2:
 		display_sql_res_info()
 	elif int(potential_action) == 3:
-		customer_check_out()
+		fetch_reservation_to_cancel()
 	elif int(potential_action) == 4:
 		fetch_reservation_to_cancel()
 	else:
